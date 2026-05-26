4f12da5fabec2ca196eda6ea5f2cd3f67b9c8e89
Kirllos Hany <kirllos.hany@epower.com.eg>
2026-05-26 15:33:30 +0300

    build(backend): add correlation middleware for T007

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppConfigModule } from './common/config/config.module';
 import { DatabaseModule } from './common/database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { CorrelationMiddleware } from './common/http/correlation.middleware';
 
 @Module({
   imports: [AppConfigModule, DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: []
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(CorrelationMiddleware).forRoutes('*');
+  }
+}
